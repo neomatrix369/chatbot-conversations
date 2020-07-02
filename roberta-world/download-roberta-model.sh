381a63f856e44a6c2793d4e3fcf2aea99af3ac80
Mani Sarkar <sadhak001@gmail.com>
2020-07-02 16:38:14 +0100

    Roberta: download the model and then run the Roberta app

--- roberta-world/download-roberta-model.sh
+++ roberta-world/download-roberta-model.sh
@@ -15,7 +15,7 @@ BASE_URL="https://dl.fbaipublicfiles.com/fairseq/models"
 TARGET_FOLDER_NAME="${MODELS_TARGET_FOLDER}/$(basename ${DOWNLOAD_FILENAME%.*.*})"
 
 if [[ -e "${TARGET_FOLDER_NAME}" ]]; then
-    echo "${TARGET_FOLDER_NAME} already exists, skipping process. Delete the folder to create it again."
+    echo "Warning: ${TARGET_FOLDER_NAME} already exists, skipping process. Delete the folder to create it again."
     exit -1
 fi
 
@@ -25,7 +25,8 @@ if [[ ! -s "${MODELS_TARGET_FOLDER}/${DOWNLOAD_FILENAME}" ]]; then
     curl -JLO "${BASE_URL}/${DOWNLOAD_FILENAME}"
     mv "${DOWNLOAD_FILENAME}" ${MODELS_TARGET_FOLDER}
 else
-    echo "${DOWNLOAD_FILENAME} already exists, proceeding with unzipping. Delete the file to download it again."
+    echo "Warning: ${DOWNLOAD_FILENAME} already exists, proceeding with unzipping. Delete the file to download it again."
 fi
 
 tar -C ${MODELS_TARGET_FOLDER} -zxvf "${MODELS_TARGET_FOLDER}/${DOWNLOAD_FILENAME}"
+echo "Finished unpacking ${DOWNLOAD_FILENAME} in the ${MODELS_TARGET_FOLDER} folder."
--- roberta-world/run-roberta-world.sh
+++ roberta-world/run-roberta-world.sh
@@ -4,4 +4,8 @@ set -e
 set -u
 set -o pipefail
 
+echo "Before running Roberta let's download the necessary model."
+./download-roberta-model.sh || true
+
+echo "Running Roberta now that the necessary model is available."
 FLASK_APP=flask-torch-app.py flask run -p 7070
