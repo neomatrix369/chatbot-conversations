69dd68fde7678833711df825e8d081199fc9e368
Yugo Sakamoto <yugoccp@gmail.com>
2022-05-12 00:04:46 -0300

    feat: add quarkus-ws-world to connect with chatbot-hub

---
+++ quarkus-ws-world/src/main/java/org/acme/AppStartup.java
@@ -0,0 +1,41 @@
+package org.acme;
+
+import javax.websocket.ContainerProvider;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.websocket.DeploymentException;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.IOException;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.Startup;
+
+
+@Startup
+@ApplicationScoped
+public class AppStartup {
+
+    @ConfigProperty(name = "chat.hub.username") 
+    String username;
+
+    @ConfigProperty(name = "chat.hub.host") 
+    String host;
+
+    void onStart(@Observes StartupEvent ev) {               
+        // Register websocket consumers as soon the application starts
+        try {
+            ContainerProvider.getWebSocketContainer().connectToServer(ChatClient.class, new URI(host + username));
+        } catch (DeploymentException | URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {               
+        System.out.println("The application is stopping...");
+    }
+}
---
+++ quarkus-ws-world/src/main/java/org/acme/ChatClient.java
@@ -0,0 +1,40 @@
+package org.acme;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.OnClose;
+import javax.websocket.Session;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ClientEndpoint
+public class ChatClient {
+    
+    private static final String COMPUTER_NAME = "Quarkus";
+
+    private Eliza _eliza = new Eliza(COMPUTER_NAME);
+
+    @ConfigProperty(name = "chat.hub.username") 
+    String username;
+   
+    @OnOpen
+    public void open(Session session) {
+        System.out.println("Connected!");
+    }
+
+    @OnMessage
+    public void message(String msg, Session session) {
+        System.out.println(msg);
+        // Ignore self message and only answer others messages.
+        if (!msg.startsWith(username)) {
+            String answer = _eliza.ask(msg);
+            session.getAsyncRemote().sendText(answer);
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println(String.format("Session %s close", session.getId()));
+    }
+}
