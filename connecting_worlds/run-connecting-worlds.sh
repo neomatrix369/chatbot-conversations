cf8bb1dbc56355b39055567110401c27e9543f74
Yugo Sakamoto <yugoccp@gmail.com>
2022-05-25 00:25:08 -0300

    feat: replace connecting_worlds with websocket server

--- connecting_worlds/run-connecting-worlds.sh
+++ connecting_worlds/run-connecting-worlds.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Copyright 2020 Mani Sarkar
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -e
 set -u
 set -o pipefail
@@ -24,5 +10,4 @@ if [[ ! -z "${FILES_CHANGED}" ]]; then
 fi
 
 clear
-WORLDS="${1:-}"
-java -jar target/connecting_worlds-1.0-SNAPSHOT-shaded.jar "${WORLDS}"
+java -jar target/quarkus-app/quarkus-run.jar
