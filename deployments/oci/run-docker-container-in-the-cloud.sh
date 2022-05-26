019ff591435ed4a6cd0c3a4f220339c072406b5c
Mani Sarkar <sadhak001@gmail.com>
2022-05-26 19:57:51 +0100

    OCI: fixed the issue with aborting execution when the run-docker-container-in-the-cloud.sh command is executed.

--- deployments/oci/run-docker-container-in-the-cloud.sh
+++ deployments/oci/run-docker-container-in-the-cloud.sh
@@ -9,7 +9,7 @@ echo "Public IP address of the cloud instance running is ${INSTANCE_PUBLIC_IP}"
 
 exit_code=0
 ssh opc@${INSTANCE_PUBLIC_IP} \
-    'cd chatbot-conversations; ./docker-runner.sh --runContainer' \
+    -t 'cd chatbot-conversations; ./docker-runner.sh --runContainer' \
     || exit_code=$? && true
 
 if [[ ${exit_code} -eq 0 ]]; then
@@ -19,6 +19,7 @@ if [[ ${exit_code} -eq 0 ]]; then
 else
 	echo ""
 	echo "Failed trying to run the docker container, its possible it has already been executed, check the error logs above."
-	echo "Or try to ssh into the container and investigate."
+	echo "Or try to ssh into the container and investigate. Checkout README to find out debugging steps "
+	echo "(hints: get-instance-public-ip.sh and ssh onto the remote VM)."
 	echo ""
 fi
