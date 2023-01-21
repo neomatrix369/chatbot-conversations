5bc8e7474d87c299b231afa169d748d743d49050
yugoccp <yugoccp@gmail.com>
2023-01-20 21:23:30 -0300

    update init.sh to check linux OS version

--- deployments/oci/init.sh
+++ deployments/oci/init.sh
@@ -6,6 +6,16 @@ groupadd docker
 usermod -aG docker opc
 
 # Install docker
-yum install -y docker-engine
+if $(uname -o) == "GNU/Linux"; then
+    echo 'Running on CentOS'
+    yum install -y docker-engine
+else 
+    echo 'Running on Oracle Linux or another Linux'
+    sudo dnf install -y dnf-utils zip unzip
+    sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    sudo dnf remove -y runc
+    sudo dnf install -y docker-ce --nobest
+fi
+
 systemctl start docker
 systemctl enable docker
