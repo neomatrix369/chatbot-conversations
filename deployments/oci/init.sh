5de2009341f4088f94383726f858295574e5196d
yugoccp <yugoccp@gmail.com>
2023-05-01 16:06:26 -0300

    fix: change init.sh to install docker with opc user and add docker status in the end for helpful log

--- deployments/oci/init.sh
+++ deployments/oci/init.sh
@@ -8,10 +8,10 @@ usermod -aG docker opc
 # Install docker
 if [[ "$(cat /etc/oracle-release || true)" == "Oracle Linux Server release 8.6" ]]; then
     echo 'Running on Oracle Linux 8.6'
-    sudo dnf install -y dnf-utils zip unzip
-    sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-    sudo dnf remove -y runc
-    sudo dnf install -y docker-ce --nobest
+    dnf install -y dnf-utils zip unzip
+    dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+    dnf remove -y runc
+    dnf install -y docker-ce --nobest
 else 
     echo 'Running on CentOS'
     yum install -y docker-engine
@@ -19,3 +19,4 @@ fi
 
 systemctl start docker
 systemctl enable docker
+systemctl status docker
